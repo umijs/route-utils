ace54fadb47e1238f9dffe964584ce324ed43070
chenshuai2144 <qixian.cs@outlook.com>
2020-05-06 19:08:56 +0800

    🐲 : export MenuDataItem type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { MenuDataItem } from './types';
+
 export { default as transformRoute } from './transformRoute/transformRoute';
 export { default as getFlatMenus } from './getFlatMenus/getFlatMenus';
 export { default as getMatchMenu } from './getMatchMenu/getMatchMenu';
+
+export type { MenuDataItem }
