fd19c5ffdba1b54b5409aa88ee50f355ba386035
Ivan <37790387+Ivan-zyl@users.noreply.github.com>
2021-11-15 11:23:03 +0800

    Prevent the children of the parent from bringing to the routes (#9)

--- src/transformRoute/transformRoute.ts
+++ src/transformRoute/transformRoute.ts
@@ -204,6 +204,7 @@ function formatter(
         // eslint-disable-next-line @typescript-eslint/naming-convention
         pro_layout_parentKeys = [],
         routes,
+        children,
         icon,
         flatMenu,
         indexRoute,
